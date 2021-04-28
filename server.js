15b20e181117aaac1f2cbcc74be674329ee103c2
Michal S <michalsienczuk@gmail.com>
2021-04-28 23:41:46 +0200

    2nd endpoint

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('<h1>My first server!</h1>');
 });
 
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact</h1>');
+});
+
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
 });
